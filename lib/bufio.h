bf13ad9d36b0f8cd79eda637b3d9faae4c14b56a
flyingleafe <flyingleafe@gmail.com>
2015-03-31 00:25:48 +0300

    hw4: added header file

---
+++ lib/bufio.h
@@ -0,0 +1,15 @@
+#ifndef FLF_OS_BUFIO
+#define FLF_OS_BUFIO
+
+struct buf_t;
+typedef struct buf_t buf_t;
+typedef int fd_t;
+
+buf_t *buf_new(size_t capacity);
+void buf_free(struct buf_t *);
+size_t buf_capacity(buf_t *);
+size_t buf_size(buf_t *);
+ssize_t buf_fill(fd_t fd, buf_t *buf, size_t required);
+ssize_t buf_flush(fd_t fd, buf_t *buf, size_t required);
+
+#endif
