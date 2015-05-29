27261c6c8d3f3682eaaebf2b1302dcd809bd7832
flyingleafe <flyingleafe@gmail.com>
2015-05-29 17:57:08 +0400

    imported bufio functions from defence

--- lib/bufio.h
+++ lib/bufio.h
@@ -11,5 +11,7 @@ size_t buf_capacity(buf_t *);
 size_t buf_size(buf_t *);
 ssize_t buf_fill(fd_t fd, buf_t *buf, size_t required);
 ssize_t buf_flush(fd_t fd, buf_t *buf, size_t required);
+ssize_t buf_getline(fd_t fd, buf_t *buf, char *dest);
+ssize_t buf_write(fd_t fd, buf_t *buf, char *src, size_t len);
 
 #endif
