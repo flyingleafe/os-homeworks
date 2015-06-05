eb5b941d43a598f2c43163726fa4fc0b00a5d24f
flyingleafe <flyingleafe@gmail.com>
2015-06-05 05:46:26 +0000

    polling: works somehow, but really buggy

--- bipiper/polling.c
+++ bipiper/polling.c
@@ -9,8 +9,8 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 typedef struct {
     buf_t* in;
@@ -31,10 +31,21 @@ void pipebuf_free(pipebuf *pb) {
 
 struct pollfd pollfds[256];
 pipebuf buffs[127];
+char valid_in[256];
+char valid_out[256];
 int count = 2;
 
+// Dual index - +1 if even, -1 if odd
+int dual(int i) {
+    return i + 1 - (i & 1) * 2;
+}
+
+// Lead index - round by 2
+int lead(int i) {
+    return i & ~1;
+}
+
 void add_pair(int fd1, int fd2) {
-    assert(count <= 254);
 
     pollfds[count].fd = fd1;
     pollfds[count + 1].fd = fd2;
@@ -43,30 +54,51 @@ void add_pair(int fd1, int fd2) {
     buffs[(count - 2) / 2] = pipebuf_new(4096);
     count += 2;
 
-    // stop ignoring both ports
+    // stop ignoring both acceptfds
     pollfds[0].fd = abs(pollfds[0].fd);
     pollfds[1].fd = abs(pollfds[1].fd);
 }
 
-void remove_pair(int index) {
-    assert(index % 2 == 0);
-
-    pipebuf_free(&buffs[index / 2]);
-    memmove(pollfds + index, pollfds + index + 2, count - index - 2);
-
+int remove_pair(int index) {
+    index = lead(index);
     count -= 2;
+
+    // Swap fds
+    struct pollfd tmp = pollfds[index];
+    pollfds[index] = pollfds[count];
+    pollfds[count] = tmp;
+    tmp = pollfds[index + 1];
+    pollfds[index + 1] = pollfds[count + 1];
+    pollfds[count + 1] = tmp;
+
+    // Swap valids
+    char tmpf = valid_in[index];
+    valid_in[index] = valid_in[count];
+    valid_in[count] = tmpf;
+    tmpf = valid_out[index];
+    valid_out[index] = valid_out[count];
+    valid_out[count] = tmpf;
+
+    // Swap buffs
+    pipebuf tmpb = buffs[(index - 2) / 2];
+    buffs[(index - 2) / 2] = buffs[(count - 2) / 2];
+
+    pipebuf_free(&tmpb);
+    RETHROW_IO(close(pollfds[count].fd));
+    RETHROW_IO(close(pollfds[count + 1].fd));
+    return 0;
 }
 
 int accept_from(int i, int* remote1, int* remote2) {
     // something terrible happened
     if (pollfds[i].revents != POLLIN) {
-        return 1;
+        return -1;
     }
     *remote1 = accept4(pollfds[i].fd, NULL, NULL, SOCK_NONBLOCK);
     if (*remote1 < 0) {
         if (errno != EWOULDBLOCK) {
             perror("accept");
-            return 1;
+            return -1;
         }
     } else {
         if (*remote2 > 0) {
@@ -79,6 +111,82 @@ int accept_from(int i, int* remote1, int* remote2) {
     }
 }
 
+int read_sock(int i) {
+    buf_t* buf;
+    if (i % 2 == 0) {
+        buf = buffs[(i - 2) / 2].in;
+    } else {
+        buf = buffs[(i - 2) / 2].out;
+    }
+
+    size_t prev_size = buf_size(buf);
+    int res = buf_fill(pollfds[i].fd, buf, 1);
+    RETHROW_IO(res);
+
+    printf("filled buf: %d\n", res);
+    // EOF
+    if (res == prev_size) {
+        // stop pollin
+        pollfds[i].events &= ~POLLIN;
+        valid_in[i] = 0;
+        RETHROW_IO(shutdown(pollfds[i].fd, SHUT_RD));
+
+        if (!valid_out[i] || !valid_in[dual(i)]) {
+            RETHROW_IO(remove_pair(i));
+        }
+
+        return 0;
+    }
+
+    // allow pollout for sink
+    if (valid_out[dual(i)]) {
+        pollfds[dual(i)].events |= POLLOUT;
+    }
+
+    // restrict pollin for source
+    if (res == buf_capacity(buf)) {
+        pollfds[i].events &= ~POLLIN;
+    }
+    return res - prev_size;
+}
+
+int write_sock(int i) {
+    buf_t* buf;
+    if (i % 2 == 0) {
+        buf = buffs[(i - 2) / 2].out;
+    } else {
+        buf = buffs[(i - 2) / 2].in;
+    }
+
+    printf("we have in buf: %d\n", buf_size(buf));
+    int res = buf_flush(pollfds[i].fd, buf, buf_size(buf));
+    RETHROW_IO(res);
+
+    printf("wrote buf: %d\n", res);
+    // Error occured
+    if (res == 0) {
+        pollfds[i].events &= ~POLLOUT;
+        valid_out[i] = 0;
+        RETHROW_IO(shutdown(pollfds[i].fd, SHUT_WR));
+
+        if (!valid_in[i] || !valid_out[dual(i)]) {
+            RETHROW_IO(remove_pair(i));
+        }
+
+        return 0;
+    }
+
+    // allow pollin for source
+    if (valid_in[dual(i)]) {
+        pollfds[dual(i)].events |= POLLIN;
+    }
+
+    // restrict pollout for sink
+    if (buf_size(buf) == 0) {
+        pollfds[i].events &= ~POLLOUT;
+    }
+    return res;
+}
 
 int main(int argc, char *argv[])
 {
@@ -90,10 +198,13 @@ int main(int argc, char *argv[])
     int sock1, sock2;
     CATCH_IO(sock1 = make_server_socket(argv[1]));
     CATCH_IO(sock2 = make_server_socket(argv[2]));
-    CATCH_IO(fcntl(sock1, F_SETFL, O_NONBLOCk));
+    CATCH_IO(fcntl(sock1, F_SETFL, O_NONBLOCK));
     CATCH_IO(fcntl(sock2, F_SETFL, O_NONBLOCK));
     CATCH_IO(listen(sock1, 1));
     CATCH_IO(listen(sock2, 1));
+
+    memset(valid_in, 1, sizeof(valid_in));
+    memset(valid_out, 1, sizeof(valid_out));
 
     pollfds[0].fd = sock1;
     pollfds[1].fd = sock2;
@@ -104,18 +215,28 @@ int main(int argc, char *argv[])
 
     while (1) {
         CATCH_IO(poll(pollfds, count, -1));
+        printf("got poll\n");
         if (pollfds[0].revents > 0) {
-            accept_from(0, &remote1, &remote2);
+            CATCH_IO(accept_from(0, &remote1, &remote2));
+            printf("accepted 1\n");
         }
         if (pollfds[1].revents > 0) {
-            accept_from(1, &remote2, &remote1);
+            CATCH_IO(accept_from(1, &remote2, &remote1));
+            printf("accepted 2\n");
         }
 
         int i;
         for (i = 2; i < count; i++) {
-            if (pollfds[i].revents > 0) {
-                if (pollfds[i].revents & POLLIN != 0) {
-
+            if (pollfds[i].revents != 0) {
+                if ((pollfds[i].revents & POLLIN) != 0) {
+                    printf("read from %d-th sock, %d\n", i, pollfds[i].revents);
+                    CATCH_IO(read_sock(i));
+                    printf("read!\n");
+                }
+                if ((pollfds[i].revents & POLLOUT) != 0) {
+                    printf("write to %d-th sock, %d\n", i, pollfds[i].revents);
+                    CATCH_IO(write_sock(i));
+                    printf("write!\n");
                 }
             }
         }
